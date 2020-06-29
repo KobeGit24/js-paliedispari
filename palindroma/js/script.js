d07f0f7f4b51a2a6b3294658bbe4ab522609edbc
Stefano <stefano.zaccaria29@gmail.com>
2020-06-29 17:07:49 +0200

    palindroma complete!

---
+++ palindroma/js/script.js
@@ -0,0 +1,32 @@
+//PALINDROMA
+var word = prompt('inserisci una parola:');
+reverse(word);
+
+// for (var i = userWord.length -1; i >= 0; i--) {
+//   wordReverse+=userWord[i];   
+// }
+// console.log(wordReverse);
+
+// if (userWord == wordReverse) {
+//     console.log('la parola e palindroma');  
+// } else {
+//     console.log('la parola non e palindroma');   
+// }
+
+function reverse(userWord) {
+    var wordReverse = "";
+    for (var i = userWord.length -1; i >= 0; i--) {
+        wordReverse+=userWord[i];   
+    } 
+    if (userWord == wordReverse) {
+        console.log('la parola e palindroma');  
+    } else {
+        console.log('la parola non e palindroma');   
+    }
+    return;
+}
+
+
+// PARI E DISPARI
+
+var user = prompt('inserisci una parola');
