0293ad599ab1243db393d589587c67f6abb98c3f
Stefano <stefano.zaccaria29@gmail.com>
2020-06-30 11:04:51 +0200

    update!

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -2,11 +2,8 @@
 do {
     var choice = prompt('scegli tra pari e dispari');
 } while ( choice!='pari' && choice!='dispari')
-
 var userNumber = parseInt(prompt('inserisci un numero da 1 a 5'));
 var computer = random(1,5);
-console.log(computer);
-
 var result = computer + userNumber;
 EvenOdd(choice,result);
 
